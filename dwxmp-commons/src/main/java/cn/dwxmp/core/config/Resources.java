194a114591a7ecca91ab3c49221e3296eade3bdb
huxinsheng <hxsysh@gmail.com>
2017-09-20 14:16:49 +0800

    修改打包目录、及打包方式

--- dwxmp-commons/src/main/java/cn/dwxmp/core/config/Resources.java
+++ dwxmp-commons/src/main/java/cn/dwxmp/core/config/Resources.java
@@ -1,7 +1,10 @@
 package cn.dwxmp.core.config;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.context.i18n.LocaleContextHolder;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -9,17 +12,26 @@ import java.util.ResourceBundle;
 
 /**
  * 加载配置
- * 
+ *
  * @author HuXinsheng
  * @version 2016年5月20日 下午3:19:19
  */
 public final class Resources {
-    /** 第三方登录配置 */
-    public static final ResourceBundle THIRDPARTY = ResourceBundle.getBundle("config/thirdParty");
-    /** 国际化信息 */
+    protected static final Logger logger = LogManager.getLogger(Resources.class);
+    /**
+     * 第三方登录配置
+     */
+    @Resource
+    public static ResourceBundle THIRDPARTY;
+    //= ResourceBundle.getBundle(System.getProperty("spring.profiles.active") + "/thirdParty");
+    /**
+     * 国际化信息
+     */
     private static final Map<String, ResourceBundle> MESSAGES = new HashMap<>();
 
-    /** 国际化信息 */
+    /**
+     * 国际化信息
+     */
     public static String getMessage(String key, Object... params) {
         Locale locale = LocaleContextHolder.getLocale();
         ResourceBundle message = MESSAGES.get(locale.getLanguage());
@@ -38,7 +50,9 @@ public final class Resources {
         return message.getString(key);
     }
 
-    /** 清除国际化信息 */
+    /**
+     * 清除国际化信息
+     */
     public static void flushMessage() {
         MESSAGES.clear();
     }
